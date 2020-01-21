e8632abb8c6abb526916e3053603a43f0def20ca
byetheway@me.com <byetheway@me.com>
2020-01-21 09:58:12 +0800

    Project Organization & Architecture

---
+++ GHFollowers/Custom Views/GFAlertContainerView.swift
@@ -0,0 +1,31 @@
+//
+//  GFAlertContainerView.swift
+//  GHFollowers
+//
+//  Created by YuLung Chen on 2020/1/21.
+//  Copyright © 2020 YuLung Chen. All rights reserved.
+//
+
+import UIKit
+
+class GFAlertContainerView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        backgroundColor       = .systemBackground
+        layer.cornerRadius    = 16
+        layer.borderWidth     = 2
+        layer.borderColor     = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
